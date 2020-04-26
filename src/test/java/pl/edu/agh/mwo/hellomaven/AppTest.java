195146190b0e8509f6934239c70d447c2890cfa8
Tymoteusz <tymoteuszsloninka@interia.pl>
2020-04-26 09:12:16 +0200

    first commit

---
+++ src/test/java/pl/edu/agh/mwo/hellomaven/AppTest.java
@@ -0,0 +1,15 @@
+package pl.edu.agh.mwo.hellomaven;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    /**
+     * Sample test.
+     */
+    @Test
+    public void sampleTest() {
+        Assert.assertTrue(true);
+    }
+}
